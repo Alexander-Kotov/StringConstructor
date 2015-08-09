1ac9403acf081a4c168ca455093bf6f2263b3104
Alexander Kotov <alexander_kotov@pochtamt.ru>
2015-08-09 17:53:58 +0300

    [+] тест на подстановки

--- tests/StringConstructor.cpp
+++ tests/StringConstructor.cpp
@@ -41,10 +41,39 @@ TEST(StringConstructor, template_2)
     StringConstructor sc("...${ABC}...${DEF}...", registry);
 }
 
+TEST(StringConstructor, template_3)
+{
+    mocks::GeneratorRegistry registry;
+    EXPECT_CALL(registry, getGenerator(_)).
+        Times(0);
+    StringConstructor sc("...", registry);
+}
+
 TEST(StringConstructor, invalid_template)
 {
     mocks::GeneratorRegistry registry;
     EXPECT_CALL(registry, getGenerator(_)).
         Times(0);
     StringConstructor sc("${ABC", registry);
+}
+
+TEST(StringConstructor, construct)
+{
+    auto printXyz = [](std::ostream &s) {
+        s << "XYZ";
+    };
+
+    auto generator = std::make_shared<mocks::Generator>();
+    EXPECT_CALL(*generator, generateTo(_)).
+        WillRepeatedly(Invoke(printXyz));
+
+    mocks::GeneratorRegistry registry;
+    EXPECT_CALL(registry, getGenerator(StringView("ABC"))).
+        WillRepeatedly(Return(generator));
+
+    std::ostringstream s;
+    StringConstructor sc(">>>${ABC}<<<", registry);
+    sc.constructTo(s);
+
+    ASSERT_EQ(">>>XYZ<<<", s.str());
 }
